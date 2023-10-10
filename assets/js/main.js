cff84512b8a56ebafb9e350249a96828cc0b4ff9
MuhammadHarisNugroho <146704062+MuhammadHarisNugroho@users.noreply.github.com>
2023-10-10 18:16:41 +0700

    Add Playlist Putra

---
+++ assets/js/main.js
@@ -0,0 +1,182 @@
+new Vue({
+    el: "#app",
+    data() {
+      return {
+        audio: null,
+        circleLeft: null,
+        barWidth: null,
+        duration: null,
+        currentTime: null,
+        isTimerPlaying: false,
+        tracks: [
+                  {
+            name: "What's up, people?!",
+            artist: "MAXIMUM THE HORMONE",
+            cover: "./assets/img/whats-up-people.jpg",
+            source: "./assets/mp3/Whats-up-people.mp3",
+            url: "https://www.youtube.com/watch?v=muoWDA6zmsY",
+            favorited: false
+          },
+          {
+            name: "ChuChu Lovely MuniMuni MuraMura PrinPrin Boron Nururu ReroRero",
+            artist: "MAXIMUM THE HORMONE",
+            cover: "./assets/img/whats-up-people.jpg",
+            source: "./assets/mp3/ChuChu-Lovely-MuniMuni-MuraMura-PrinPrin-Boron-Nururu-ReroRero.mp3",
+            url: "https://www.youtube.com/watch?v=MPUCEz-RCg0",
+            favorited: false
+          },
+          {
+            name: "I Really Want to Stay at Your House",
+            artist: "Rosa Walton & Hallie Coggins",
+            cover: "./assets/img/i-really-want-to-stay-at-your-home.jpg",
+            source: "./assets/mp3/I-REALLY-WANT-TO-STAY-AT-YOUR-HOUSE-Rosa-Walton-Hallie-Coggins.mp3",
+            url: "",
+            favorited: false
+          },
+  
+          {
+            name: "Lost Kitten",
+            artist: "METRIC",
+            cover: "./assets/img/metric.jpg",
+            source: "./assets/mp3/METRIC-Lost-Kitten.mp3",
+            url: "https://www.youtube.com/watch?v=3Xw-9OE1j-Y",
+            favorited: false
+          },
+  
+          {
+            name: "One Last Kiss",
+            artist: "Hikaru Utada",
+            cover: "./assets/img/one-last-kiss.jpg",
+            source: "./assets/mp3/One-Last-Kiss.mp3",
+            url: "https://www.youtube.com/watch?v=0Uhh62MUEic",
+            favorited: false
+          }
+        ],
+        currentTrack: null,
+        currentTrackIndex: 0,
+        transitionName: null
+      };
+    },
+    methods: {
+      play() {
+        if (this.audio.paused) {
+          this.audio.play();
+          this.isTimerPlaying = true;
+        } else {
+          this.audio.pause();
+          this.isTimerPlaying = false;
+        }
+      },
+      generateTime() {
+        let width = (100 / this.audio.duration) * this.audio.currentTime;
+        this.barWidth = width + "%";
+        this.circleLeft = width + "%";
+        let durmin = Math.floor(this.audio.duration / 60);
+        let dursec = Math.floor(this.audio.duration - durmin * 60);
+        let curmin = Math.floor(this.audio.currentTime / 60);
+        let cursec = Math.floor(this.audio.currentTime - curmin * 60);
+        if (durmin < 10) {
+          durmin = "0" + durmin;
+        }
+        if (dursec < 10) {
+          dursec = "0" + dursec;
+        }
+        if (curmin < 10) {
+          curmin = "0" + curmin;
+        }
+        if (cursec < 10) {
+          cursec = "0" + cursec;
+        }
+        this.duration = durmin + ":" + dursec;
+        this.currentTime = curmin + ":" + cursec;
+      },
+      updateBar(x) {
+        let progress = this.$refs.progress;
+        let maxduration = this.audio.duration;
+        let position = x - progress.offsetLeft;
+        let percentage = (100 * position) / progress.offsetWidth;
+        if (percentage > 100) {
+          percentage = 100;
+        }
+        if (percentage < 0) {
+          percentage = 0;
+        }
+        this.barWidth = percentage + "%";
+        this.circleLeft = percentage + "%";
+        this.audio.currentTime = (maxduration * percentage) / 100;
+        this.audio.play();
+      },
+      clickProgress(e) {
+        this.isTimerPlaying = true;
+        this.audio.pause();
+        this.updateBar(e.pageX);
+      },
+      prevTrack() {
+        this.transitionName = "scale-in";
+        this.isShowCover = false;
+        if (this.currentTrackIndex > 0) {
+          this.currentTrackIndex--;
+        } else {
+          this.currentTrackIndex = this.tracks.length - 1;
+        }
+        this.currentTrack = this.tracks[this.currentTrackIndex];
+        this.resetPlayer();
+      },
+      nextTrack() {
+        this.transitionName = "scale-out";
+        this.isShowCover = false;
+        if (this.currentTrackIndex < this.tracks.length - 1) {
+          this.currentTrackIndex++;
+        } else {
+          this.currentTrackIndex = 0;
+        }
+        this.currentTrack = this.tracks[this.currentTrackIndex];
+        this.resetPlayer();
+      },
+      resetPlayer() {
+        this.barWidth = 0;
+        this.circleLeft = 0;
+        this.audio.currentTime = 0;
+        this.audio.src = this.currentTrack.source;
+        setTimeout(() => {
+          if(this.isTimerPlaying) {
+            this.audio.play();
+          } else {
+            this.audio.pause();
+          }
+        }, 300);
+      },
+      favorite() {
+        this.tracks[this.currentTrackIndex].favorited = !this.tracks[
+          this.currentTrackIndex
+        ].favorited;
+      }
+    },
+    created() {
+      let vm = this;
+      this.currentTrack = this.tracks[0];
+      this.audio = new Audio();
+      this.audio.src = this.currentTrack.source;
+      this.audio.ontimeupdate = function() {
+        vm.generateTime();
+      };
+      this.audio.onloadedmetadata = function() {
+        vm.generateTime();
+      };
+      this.audio.onended = function() {
+        vm.nextTrack();
+        this.isTimerPlaying = true;
+      };
+  
+      // this is optional (for preload covers)
+      for (let index = 0; index < this.tracks.length; index++) {
+        const element = this.tracks[index];
+        let link = document.createElement('link');
+        link.rel = "prefetch";
+        link.href = element.cover;
+        link.as = "image"
+        document.head.appendChild(link)
+      }
+    }
+  });
+  
